046ebf3cb353ea95e2b26420d0d076f3138ad5b9
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-16 17:13:41 +0530

    Refresh PWA cache for Module 01

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fitguide-v14';
-const ASSETS=['./','./index.html','./app.css?v=20260916-8','./app.js?v=20260916-8','./manifest.json'];
+const CACHE='fitguide-module01-v1';
+const ASSETS=['./','./index.html','./module-01.css?v=1','./module-01.js?v=1','./manifest.json'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});

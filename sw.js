4f4b883d00911577c4c096aedd85fa3c1d8ba0d1
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-16 16:30:28 +0530

    Refresh app shell cache for v3 architecture

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fitguide-v12';
-const ASSETS=['./','./index.html','./app.css?v=20260916-6','./train-v6.css?v=1','./app.js?v=20260916-6','./train-v6.js?v=1','./manifest.json'];
+const CACHE='fitguide-v13';
+const ASSETS=['./','./index.html','./app.css?v=20260916-7','./fitness-v3.css?v=20260916-1','./app.js?v=20260916-v3','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});

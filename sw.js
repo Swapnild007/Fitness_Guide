ca00921e20172066ec0fa9962f8bf600190b4b3b
Swapnild007 <swapneeldalvi3@gmail.com>
2026-09-16 16:10:33 +0530

    Bump cache for corrected Train renderer

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='fitguide-v9';
-const ASSETS=['./','./index.html','./app.css?v=20260916-6','./train-v4.css?v=20260916-3','./train-v4-next.css?v=20260916-1','./app.js?v=20260916-6','./train-v4.js?v=20260916-6','./train-live.js?v=1','./manifest.json'];
+const CACHE='fitguide-v10';
+const ASSETS=['./','./index.html','./app.css?v=20260916-6','./train-v4.css?v=20260916-3','./train-v4-next.css?v=20260916-1','./app.js?v=20260916-6','./train-v4.js?v=20260916-6','./train-live.js?v=2','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))))});
